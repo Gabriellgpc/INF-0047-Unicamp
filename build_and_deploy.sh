71a38e92b82b84a36f1f34a76c7b670bf802b745
Luis Gabriel Condados-CONDADOS <condados@motorola.com>
2022-12-16 17:49:27 -0300

    update

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -8,3 +8,14 @@
 docker build . -t t5-translation-fastapi:latest
 
 docker run -it --rm --p 8000:8000 t5-translation-fastapi:latest serve
+
+# gunicorn --timeout 300 -k sync -b unix:/tmp/gunicorn.sock -w 2 wsgi:app --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+
+# 'gunicorn',
+#                                  '--timeout', str(model_server_timeout),
+#                                  '-k', 'sync',
+#                                  '-b', 'unix:/tmp/gunicorn.sock',
+#                                  '-w', str(model_server_workers),
+#                                  'wsgi:app',
+#                                  '--worker-class','uvicorn.workers.UvicornWorker',
+#                                  '--bind', '0.0.0.0:8000'
